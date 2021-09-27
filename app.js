3571529fb0ca1eed0db00f3d22f16e23dbcf6b56
Tsukyko <zoeamat.pro@gmail.com>
2021-09-27 15:19:43 +0200

    Untrack files in .gitignore

--- app.js
+++ app.js
@@ -13,16 +13,30 @@ app.use(express.static('public'));
 
 app.get('/api/categories', (req, res, next) => {
     const filepath = "./public/assets/categories";
+    const changePosition = ["body", "eyes", "eyebrows", "nose", "mouth"];
+    const changeColor = ["body", "eyes", "eyebrows", "mouth", "hair-fringe", "hair-side", "hair-back", "hair-top", "hair-spike", "clothes"];
+    const isMandatory = ["body", "jaw", "eyes", "eyebrows", "nose", "mouth", "hair-fringe", "clothes", "background-color"];
+
     fs.readdir(filepath, (err, files) => {
-      const categories = files.map((file) => (
-        {
-          id: file,
-          imageUrl: `http://localhost:3001/assets/categories/${file}`,
-        }
-      ));
+      const categories = files.map((file) => { 
+
+        let fileName = file.substr(3).slice(0, -4);
+
+        return (
+          {
+            id: fileName,
+            imageUrl: `http://localhost:3001/assets/categories/${file}`,
+            changePosition : changePosition.includes(fileName),
+            changeColor : changeColor.includes(fileName),
+            isMandatory : isMandatory.includes(fileName),
+          }
+        )
+      });
+
       const response = {
         categories,
       }
+
       res.status(200).json(response);
     });
 });
@@ -34,13 +48,14 @@ app.get('/api/items/:category', (req, res, next) => {
 
     fs.readdir(filepath, (err, files) => {
       if (err) {
-        console.log(err);
+        res.status(404).send('The category selected doesn\'t have any image yet !');
       }
       else {
         const categoryImages = files.map((file) => (
           {
             id: file,
             imageUrl: `http://localhost:3001/assets/${categoryCapitalized}/${file}`,
+            category: category.toLocaleLowerCase(),
           }
         ));
         const response = { categoryImages };
@@ -48,9 +63,5 @@ app.get('/api/items/:category', (req, res, next) => {
       }
     });
 });
-
-// const response = {
-//   items: [body1, body2, body3, body4, body5]
-// }
 
 module.exports = app;
